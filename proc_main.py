d436647c0dd522f8ecd097b799f68c8326a4600b
Hooges <jonathanhooges@gmail.com>
2015-11-09 14:54:28 -0500

    Update proc_main.py

--- proc_main.py
+++ proc_main.py
@@ -6,7 +6,8 @@ import mahotas as mh
 
 def blob(binary_frame,frame):
 
-	_, contours, _ = cv2.findContours(binary_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	#_, contours, _ = cv2.findContours(binary_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	contours, _ = cv2.findContours(binary_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	
 	for i, c in enumerate(contours):
 		area = cv2.contourArea(c)
@@ -73,7 +74,8 @@ def main():
 
 	ret, frame = cap.read()
 
-
+	objectx_old = []
+	objecty_old = []
 	
 	while(cap.isOpened()):
 
@@ -82,40 +84,70 @@ def main():
 		binary_frame = skin2BW(frame)
 		
 		frame, contours = blob(binary_frame,frame)
-		
-		objectx = []
-		objecty = []
-		
-		for i, c in enumerate(contours):
-			area = cv2.contourArea(c)
 
-			if area > 1000:
+		if not ret == False:
+		
+			objectx = []
+			objecty = []
+		
+			for i, c in enumerate(contours):
+				area = cv2.contourArea(c)
+
+				if area > 1000:
 			
-				cent = cv2.moments(c)
-				temp = cent['m00']
-				if not temp == 0:
-					cx = int(cent['m10']/cent['m00'])
-					cy = int(cent['m01']/cent['m00'])
-					cv2.circle(frame,(cx,cy),5,(0,0,255),-2)
-					objectx.append(cx)
-					objecty.append(cy)
-					#print objectx, objecty
-		#os.system("pause")
-		
-		#if not objectx_old == 0:
-		#	for i in objectx
+					cent = cv2.moments(c)
+					temp = cent['m00']
+					if not temp == 0:
+						cx = int(cent['m10']/cent['m00'])
+						cy = int(cent['m01']/cent['m00'])
+						#cv2.circle(frame,(cx,cy),5,(0,0,255),-2)
+						objectx.append(cx)
+						objecty.append(cy)
+						#print objectx, objecty
+			#os.system("pause")
 			
-		
-		
-		#	objectx_old = objectx
-		#	objecty_old = objecty
+			objectloc = []
+			objectval = []
+			x = []
+			y = []
+			
+			if not objectx_old == []:
+				#print objectx, objecty, objectx_old, objecty_old
+				
+				for i, j in zip(objectx, objecty):
+	
+					x_dist = np.array(objectx_old)
+					x_dist = x_dist - i
+					y_dist = np.array(objecty_old)
+					y_dist = y_dist - j
+					
+					y_dist = np.square(y_dist)
+					x_dist = np.square(x_dist)
+					
+					dist = np.sqrt(x_dist+y_dist)
+					minloc = np.argmin(dist)
+					minval = dist[minloc]
+					objectloc.append(minloc)
+					objectval.append(minval)
+					x.append(i)
+					y.append(j)
+			if not objectloc == []:
+				co = len(objectloc)
+				for i in range(0, co-1):
+					if objectval[i] > 2:
+						cv2.circle(frame,(x[i],y[i]),5,(0,0,255),-2)
+			objectx_old = objectx
+			objecty_old = objecty
+
+		else:
+			break
 		
 		cv2.imshow('frame',frame)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
-	#cap.release()
-	#cv2.destroyAllWindows()
+	cap.release()
+	cv2.destroyAllWindows()
 
 	
 	
