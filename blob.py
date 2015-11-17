59ec8286e5622ead41cb6fba02770746fad73772
Hooges <jonathanhooges@gmail.com>
2015-11-17 15:19:00 -0500

    Update blob.py

--- blob.py
+++ blob.py
@@ -1,15 +1,21 @@
 import cv2
 import numpy as np
 
-def blob(binary_frame,frame):
+def blob(binary_frame,frame,area_limit):
 	
 	temp = np.copy(binary_frame)
 	_, contours, _ = cv2.findContours(temp, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	#contours, _ = cv2.findContours(binary_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	
-	for i, c in enumerate(contours):
-		area = cv2.contourArea(c)
-		if area > 1000:
-			cv2.drawContours(frame, contours, i, (255, 0, 0), 3)
 
+	for c in contours:
+		area = cv2.contourArea(c)
+		if area > area_limit:
+			hull = cv2.convexHull(c,returnPoints = False)
+			defects = cv2.convexityDefects(c,hull)
+			
+			for i in range(defects.shape[0]):
+				s,e,f,d = defects[i,0]
+				start = tuple(c[s][0])
+				end = tuple(c[e][0])
+				cv2.line(frame,start,end,[0,255,0],2)
+	
 	return frame, contours
